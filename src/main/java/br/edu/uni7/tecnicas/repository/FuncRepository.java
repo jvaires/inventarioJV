8936099dbeb0392b17f9fc42487476d79dbde325
angelocastro9 <castromatheus060@gmail.com>
2022-11-22 18:43:31 -0300

    html

--- src/main/java/br/edu/uni7/tecnicas/repository/FuncRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.uni7.tecnicas.repository;
-
-import br.edu.uni7.tecnicas.entities.Funcionario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FuncRepository extends CrudRepository<Funcionario, Integer> {
-}
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,7 @@
+function fazGet(url){
+    let request = new XMLHttpRequest();
+    request.open("GET", url, false);
+    request.send();
+    return request.responseText;
+}
+
